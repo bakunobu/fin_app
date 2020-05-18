6603e6e47fb42cc58c25baf544a049c1a12ae19d
bakunobu <bakunobu@gmail.com>
2020-05-18 18:42:19 +0300

    updateing docs function added, bson ObjectId import added

--- db_funcs.py
+++ db_funcs.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson import ObjectId
 
 
 def connect_to_db(conf):
@@ -73,3 +74,35 @@ test_doc = {
     }
 
 add_doc(test_db, test_doc)
+
+
+def update_record(collection, filt, new_data):
+    '''
+    Updates a document using .find_one_and_update()
+    !!!needs import from bson library!!!
+    
+    Args:
+    =====
+    collection: pymongo.collection.Collection class
+    a collection to work with
+    filter: dict
+    a parameter or a set of parameters to find a required document
+    new_data: dict
+    a new data to update a document with
+    
+    Returns:
+    ========
+    None: None type
+    Doesn't return anything
+    prints 'Document Updated!'
+    '''
+        collection.find_one_and_update(filt,
+                                       {'$set': new_data})
+        print('Document Updated!')
+        
+
+#an example
+
+params =  {'_id': ObjectId("5ebbd583b40171c316ee330c")}
+new_data = {'income': 9999}
+update_record(collect, params, new_data)
