6246992c5b213e326ebae4e2e4d7740a678830ca
bakunobu <bakunobu@gmail.com>
2020-05-31 18:09:06 +0300

    amortization logic test

--- pd_book_examples.py
+++ pd_book_examples.py
@@ -3,8 +3,12 @@ import numpy as np
 
 
 loan = 3000
+rate = 0.0575
+term = 14
 
 balance = loan
+
+payment = np.round(-np.pmt(rate / 12, term, loan), 2)
 
 
 df = pd.DataFrame({
@@ -15,4 +19,20 @@ df = pd.DataFrame({
     'balance': [balance] 
 })
 
-print(df)
+# print(df)
+
+
+for i in range(1, term + 1):
+    interest = round(rate / 12 * balance, 2)
+    principal = payment - interest
+    balance -= principal
+    
+    df = df.append(pd.DataFrame({'month': [i],
+                                 'payment': [payment],
+                                 'interest': [interest],
+                                 'principal': [principal],
+                                 'balance': [balance]}))
+
+
+df = df.reset_index(drop=True)
+print(df[['month', 'payment', 'interest', 'principal', 'balance']])
