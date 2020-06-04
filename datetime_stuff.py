a8c2a0187ae0b600f86757064b0e47b90351879c
bakunobu <bakunobu@gmail.com>
2020-06-04 23:11:33 +0300

    regular revenue func added, some minor bugs fixed

--- datetime_stuff.py
+++ datetime_stuff.py
@@ -31,7 +31,7 @@ def add_revenue(source='salary', amt=0, date=datetime.datetime.now(), comment=''
     'source': 'source',
     'ammount': ammount,
     'date': Timestamp(data),
-    'comment': 'comment}
+    'comment': 'comment'
     }
     
     
@@ -73,7 +73,7 @@ def add_spending(source='booze', amt=0, date=datetime.datetime.now(), comment=''
     'source': 'source',
     'ammount': ammount,
     'date': Timestamp(data),
-    'comment': 'comment}
+    'comment': 'comment'
     }
     
     
@@ -86,4 +86,51 @@ def add_spending(source='booze', amt=0, date=datetime.datetime.now(), comment=''
     
     
 # testing
-print(add_spending(source='transport', amt=100, date='21.12.2019', comment='one-way ticket'))
+#  print(add_spending(source='transport', amt=100, date='21.12.2019', comment='one-way ticket'))
+
+
+def add_reg_revenue(source='salary', amt=0, date=datetime.datetime.now(), freq='M', comment=''):
+    '''
+    Writes down revenue. Packs all the values into a dict of a given structure
+    (converts a date into pd.Timestamp() object)
+    pandas and datetime modules are required
+    
+    Args:
+    =====
+    sourse: str
+    a sourse of revenue ('salary' is a default option)
+    
+    amt: int or float
+    an ammount of money (0 is a default value)
+    
+    date: str or datetime.datetime object
+    a date (and time) a revenue was received (datetime.daetime.now() is a default value)
+    freq: str
+    frequncy of revenue gaining(a month ('M') is for default)
+    
+    comment: str
+    any comment (a blank field is a default)
+    
+    Returns:
+    my_dict: dict
+    the structure of the tictionary is:
+    {
+    'source': 'source',
+    'ammount': ammount,
+    'date': Timestamp(data),
+    'frequency': 'frequency'
+    'comment': 'comment'
+    }
+    
+    
+    '''
+    
+    return{'source': source,
+           'ammount': amt,
+           'date': pd.Timestamp(date),
+           'frequency': freq,
+           'comment': comment}
+
+
+# testing
+print(add_reg_revenue(source='salary', amt=100, date='21.12.2019', freq='M', comment='yearly bonus'))
