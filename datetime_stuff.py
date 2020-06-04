25f81138ef5ccd6660327bc0e006af73db950a1f
bakunobu <bakunobu@gmail.com>
2020-06-04 23:05:45 +0300

    spending func added

--- datetime_stuff.py
+++ datetime_stuff.py
@@ -44,4 +44,46 @@ def add_revenue(source='salary', amt=0, date=datetime.datetime.now(), comment=''
 
 
 # testing
-print(add_revenue(source='bonus', amt=100, date='21.12.2019', comment='yearly bonus'))
+# print(add_revenue(source='bonus', amt=100, date='21.12.2019', comment='yearly bonus'))
+
+def add_spending(source='booze', amt=0, date=datetime.datetime.now(), comment=''):
+    '''
+    Writes down spending. Packs all the values into a dict of a given structure
+    (converts a date into pd.Timestamp() object)
+    pandas and datetime modules are required
+    
+    Args:
+    =====
+    sourse: str
+    a sourse of revenue ('booze' is a default option)
+    
+    amt: int or float
+    an ammount of money (0 is a default value)
+    
+    date: str or datetime.datetime object
+    a date (and time) a revenue was received (datetime.daetime.now() is a default value)
+    
+    comment: str
+    any comment (a blank field is a default)
+    
+    Returns:
+    my_dict: dict
+    the structure of the tictionary is:
+    {
+    'source': 'source',
+    'ammount': ammount,
+    'date': Timestamp(data),
+    'comment': 'comment}
+    }
+    
+    
+    '''
+    
+    return{'source': source,
+           'ammount': amt,
+           'date': pd.Timestamp(date),
+           'comment': comment}
+    
+    
+# testing
+print(add_spending(source='transport', amt=100, date='21.12.2019', comment='one-way ticket'))
