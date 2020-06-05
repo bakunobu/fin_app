687774aff10ceb1f6f03c74f0147a25691f6bd03
Sergei Vakunov <bakunobu@gmail.com>
2020-06-05 16:16:49 +0300

    connection to db added

--- basic_logic.py
+++ basic_logic.py
@@ -1,9 +1,22 @@
 import os
 from dotenv import load_dotenv, find_dotenv
+from db_funcs import connect_to_db
+
 
 load_dotenv(find_dotenv())
 
+
 MY_LOG = os.environ.get('MONGO_LOG')
 MY_PASS = os.environ.get('MONGO_PASS')
+MY_SERVER = os.environ.get('MONGO_SERVER')
+MY_DB = os.environ.get('MONGO_DB')
 
-print(MY_LOG, MY_PASS)
+
+my_config = {'login': MY_LOG,
+'paswd': MY_PASS,
+'host': MY_SERVER,
+'database': MY_DB}
+
+
+my_client = connect_to_db(my_config)
+print(type(my_client))
