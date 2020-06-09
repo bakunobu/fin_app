88a2765d25982965549f7704f4d1c2c139fdbabb
Sergei Vakunov <bakunobu@gmail.com>
2020-06-09 13:40:48 +0300

    testing spending adding func

--- draught_bd.py
+++ draught_bd.py
@@ -5,12 +5,29 @@ from pymongo import MongoClient
 
 my_client = MongoClient('mongodb://localhost:27017/')
 
-my_db = my_client['app_db']
-my_col = my_db['budget']
-my_dict = {'Title': 'booze',
-           'Ammount': 99,
-           'Date': pd.Timestamp.now(),
-           'Comment': 'testing purpose'}
 
-my_col.insert_one(my_dict)
-print(my_client.list_database_names())
+typical_spending = {'Назначение': 'Анализы',
+                    'Сумма': 360,
+                    'Цель': 'Медицина',
+                    'Дата': pd.Timestamp.now(),
+                    'Комментарий': 'Анализ перед походом к врачу'}
+
+
+def single_purchase(my_data, my_collection):
+    record = my_data.get('Назначение', 'не указано'),
+    amt = my_data.get('Сумма', 0),
+    tags = my_data.get('Цель', 'не указано'),
+    date = my_data.get('Дата', pd.Timestamp.now())
+    comment = my_data.get('Комментарий', '')
+    try:
+        my_collection.insert_one({'Назначение': record,
+                                  'Сумма': amt,
+                                  'Цель': tags,
+                                  'Дата': date,
+                                  'Комментарий': comment})
+        print(f'Расход на сумму {amt} рублей добавлен!')
+    except:
+        print('Ошибка! Попробуйте еще раз')
+        
+
+single_purchase(typical_spending, my_client.app_db.budget)
