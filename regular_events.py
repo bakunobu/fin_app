e0d2ae3663036bc407ae00df61da7b333ce6ffba
Sergei Vakunov <bakunobu@gmail.com>
2020-06-13 10:01:55 +0300

    test connection func added

--- regular_events.py
+++ regular_events.py
@@ -3,3 +3,22 @@ import numpy as np
 import pymongo
 from pymongo import MongoClient
 from bson import ObjectId
+
+
+# connect to DB
+def test_connection():
+    """
+    a simple func for testing
+    
+    will be etit with some connection options
+    """
+    my_client = MongoClient('mongodb://localhost:27017/')
+    return(my_client)
+
+
+# create the first test score to start a new collection
+
+
+
+
+# create a new DB with a new collection (with a single document)
