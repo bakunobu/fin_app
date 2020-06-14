d88a381b6e2a15b335023f0107a30e72e81861cb
Sergei Vakunov <bakunobu@gmail.com>
2020-06-14 10:01:00 +0300

    regular spending added, adding func edited

--- regular_events.py
+++ regular_events.py
@@ -29,16 +29,56 @@ record = {'Description': 'A New Years\'s test spending',
 
 
 # create a new DB with a new collection (with a single document)
-def initiate_collection(record=record):
+def initiate_collection(DB_NAME, COL_NAME, record=record):
     '''
     testing inititating func (reating a DB and a collection)
+    
+    EDITED:
+    
+    Args:
+    =====
+    DB_NAME: str
+    a database name
+    
+    COL_NAME: str
+    a collection name
+    
+    record: dict
+    a document to initialize DB/collection
+    
+    Returns:
+    ========
+    None: None type
+    prints the document's ID
     '''
     my_client = test_connection()
-    my_db = my_client['spending_DB']
-    my_col = my_db['single_purch']
+    my_db = my_client[DB_NAME]
+    my_col = my_db[COL_NAME]
     record_id = my_col.insert_one(record).inserted_id
     print(record_id)
 
 # testing
+'''
+initiate_collection('my_db', 'my_col')
+'''
 
-initiate_collection()
+# creating another DB for regular spendings
+
+# an initiating record
+
+reg_date = pd.Timestamp(2020, 1, 1).normalize()
+
+reg_record = {'Description': 'charity',
+          'Amount': 10.0,
+          'Date': reg_date,
+          'Tags': ['testing', 'charity'],
+          'Regular': 'YES',
+          'Starts': reg_date,
+          'ENDS': pd.Timestamp(2020, 12, 31).normalize(),
+          'Period': 'MS',
+          'Source': 'card',
+          'comments': 'just testing'}
+
+initiate_collection('spending_DB', 'reg_spend', record=reg_record)
+
+
