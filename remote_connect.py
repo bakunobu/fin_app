29ce75b9845cdb9322a65c1e69d2cea956c2be90
Sergei Vakunov <bakunobu@gmail.com>
2020-06-15 16:55:58 +0300

    use .env approach

--- remote_connect.py
+++ remote_connect.py
@@ -1,8 +1,13 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
 
 
+load_dotenv(find_dotenv())
+
+MY_URI = os.environ.get('MONGO_PATH')
 # manual connection
-my_client = MongoClient('mongodb://uy2f3ugdkqyndqenqfaz:cvId7Wt84S7XIScyGVkc@b52fldsjjhnxmaj-mongodb.services.clever-cloud.com:27017/b52fldsjjhnxmaj')
+my_client = MongoClient(MY_URI)
 
 my_db = my_client['b52fldsjjhnxmaj']
 my_col = my_db['test_app']
