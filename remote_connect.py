cd96a29929684b2c561a9ad714ddfdf3ca92c1b5
Sergei Vakunov <bakunobu@gmail.com>
2020-06-15 17:12:17 +0300

    func for URI generator

--- remote_connect.py
+++ remote_connect.py
@@ -5,7 +5,18 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-MY_URI = os.environ.get('MONGO_PATH')
+def generate_path():
+    prefix = 'mongodb://'
+    LOG = os.environ.get('MY_LOG')
+    PAS = os.environ.get('MY_PAS')
+    HOST = os.environ.get('MY_HOST')
+    PORT = os.environ.get('MY_PORT')
+    DB = os.environ.get('MY_DB')
+    return(f'{prefix}{LOG}:{PAS}@{HOST}:{PORT}/{DB}')
+
+
+# MY_URI = os.environ.get('MONGO_PATH')
+MY_URI = generate_path()
 # manual connection
 my_client = MongoClient(MY_URI)
 
