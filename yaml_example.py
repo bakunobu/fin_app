205dfed950600f025620cd252bde29182758254d
Sergei Vakunov <bakunobu@gmail.com>
2020-06-17 22:53:01 +0300

    yaml-based calendar DF

--- yaml_example.py
+++ yaml_example.py
@@ -1,3 +1,6 @@
+import datetime
+import pandas as pd
+from report_pd_draught import get_dates
 import yaml
 
 budget = yaml.load("""
@@ -14,3 +17,26 @@ fun:
     frequency: every week on Friday and Saturday
     amount: -40
                    """)
+
+
+def build_calendar(budget):
+    TODAY = pd.Timestamp.today().normalize()
+    END = TODAY + datetime.timedelta(days=365)
+    
+    
+    calendar = pd.DataFrame(index=pd.date_range(start=TODAY,
+                                                end=END))
+    
+    for k, v in budget.items():
+        frequency = v.get('frequency')
+        amount = v.get('amount')
+        dates = get_dates(frequency)
+        i = pd.DataFrame(
+            data = {k: amount},
+            index = pd.DatetimeIndex(pd.Series(dates))
+        )
+        calendar = pd.concat([calendar, i], axis = 1).fillna(0)
+        calendar['total'] = calendar.sum(axis=1)
+        calendar['cum_total'] = calendar['total'].cumsum()
+        
+    return(calendar)
